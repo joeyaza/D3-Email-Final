5b8c9083d01e4c52684290fd0948c07fe0f55a17
joeyaza <JAzadehdel@gmail.com>
2017-05-06 14:24:44 +0100

    bar chart and donut done - tool tips in place and interaction on donut

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 ////////////////////////////////////////////////////////////
 // BAR CHART //////////////////////////////////////////////
 //////////////////////////////////////////////////////////
-
+(function(d3) {
+        'use strict';
 // set the dimensions and margins of the graph
 var margin = {
     top: 40,
@@ -9,11 +10,11 @@ var margin = {
     bottom: 35,
     left: 90
 };
-
+var dataObj;
 var width = 500 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom;
 
-var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+var tooltip = d3.select("body").append("div").attr("class", "tooltip");
 
 // set the ranges
 var y = d3.scaleBand()
@@ -104,97 +105,154 @@ var svg = d3.select("#bar-chart").append("svg")
         return d.Value;
     });
 });
-
+})(window.d3);
 ////////////////////////////////////////////////////////////
 // DONUT //////////////////////////////////////////////
 //////////////////////////////////////////////////////////
-d3.csv("data.csv", function(data) {
-  dataObj = data;
-  console.log(data)
-  var dataByCount = d3.nest()
-  .key(function(d) { return d.Country; })
-  .rollup(function(leaves){
-    return d3.sum(leaves, function(d) {
-      return d.clickedFlag;
-  });
-  }).entries(data)
-    .map(function(d){
-      return { Country: d.key, Value: d.value}
-    })
-console.log(dataByCount)
+(function(d3) {
+        'use strict';
+        var dataObj;
+        var width = 600;
+        var height = 360;
+        var radius = Math.min(width, height) / 2;
+        var donutWidth = 75;
+        var legendRectSize = 18;
+        var legendSpacing = 4;
+
+        var color = d3.scaleOrdinal(d3.schemeCategory20b);
+
+       var svg = d3.select('#donut')
+          .append('svg')
+          .attr('width', width)
+          .attr('height', height)
+          .append('g')
+          .attr('transform', 'translate(' + (width / 2) + 
+            ',' + (height / 2) + ')');
+
+        var arc = d3.arc()
+          .innerRadius(radius - donutWidth)
+          .outerRadius(radius);
+
+        var pie = d3.pie()
+          .value(function(d) { return d.Value; })
+          .sort(null);
+
+        var tooltip = d3.select('#donut')
+          .append('div')
+          .attr('class', 'tooltip');
+        
+        tooltip.append('div')
+          .attr('class', 'Country');
+
+        tooltip.append('div')
+          .attr('class', 'Value');
+
+        tooltip.append('div')
+          .attr('class', 'percent');
+
+        d3.csv("data.csv", function(data) {
+              dataObj = data;
+              var dataByCount = d3.nest()
+              .key(function(d) { return d.Country; })
+              .rollup(function(leaves){
+                return d3.sum(leaves, function(d) {
+                  return d.clickedFlag;
+              });
+              }).entries(data)
+                .map(function(d){
+                  return { Country: d.key, Value: d.value}
+                })
+          dataByCount.forEach(function(d) {
+            d.enabled = true;                                         // NEW
+          });
+
+          var path = svg.selectAll('path')
+            .data(pie(dataByCount))
+            .enter()
+            .append('path')
+            .attr('d', arc)
+            .attr('fill', function(d, i) { 
+              return color(d.data.Country); 
+            })                                                        // UPDATED (removed semicolon)
+            .each(function(d) { this._current = d; });                // NEW
+
+          path.on('mouseover', function(d) {
+            var total = d3.sum(dataByCount.map(function(d) {
+              return (d.enabled) ? d.Value : 0;                       // UPDATED
+            }));
+            var percent = Math.round(1000 * d.data.Value / total) / 10;
+            tooltip.select('.Country').html(d.data.Country);
+            tooltip.select('.Value').html(d.data.Value); 
+            tooltip.select('.percent').html(percent + '%'); 
+            tooltip.style('display', 'block');
+          });
+          
+          path.on('mouseout', function() {
+            tooltip.style('display', 'none');
+          });
 
 
+          path.on('mousemove', function(d) {
+            tooltip.style('top', (d3.event.pageY + 10) + 'px')
+              .style('left', (d3.event.pageX + 10) + 'px');
+          });
+          
+            
+          var legend = svg.selectAll('.legend')
+            .data(color.domain())
+            .enter()
+            .append('g')
+            .attr('class', 'legend')
+            .attr('transform', function(d, i) {
+              var height = legendRectSize + legendSpacing
+              var offset =  height * color.domain().length + 10;
+              var horz =   legendRectSize + 200;
+              var vert = i * height - offset - 70;
+              return 'translate(' + horz + ',' + vert + ')';
+            });
 
-var margin = {
-    top: 40,
-    right: 55,
-    bottom: 35,
-    left: 90
-};
+          legend.append('rect')
+            .attr('width', legendRectSize)
+            .attr('height', legendRectSize)                                   
+            .style('fill', color)
+            .style('stroke', color)                                   // UPDATED (removed semicolon)
+            .on('click', function(Country) {                            // NEW
+              var rect = d3.select(this);                             // NEW
+              var enabled = true;                                     // NEW
+              var totalEnabled = d3.sum(dataByCount.map(function(d) {     // NEW
+                return (d.enabled) ? 1 : 0;                           // NEW
+              }));                                                    // NEW
+              
+              if (rect.attr('class') === 'disabled') {                // NEW
+                rect.attr('class', '');                               // NEW
+              } else {                                                // NEW
+                if (totalEnabled < 2) return;                         // NEW
+                rect.attr('class', 'disabled');                       // NEW
+                enabled = false;                                      // NEW
+              }                                                       // NEW
 
-var width = 500 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-    legendRectSize = 18,                         
-    legendSpacing = 4;   
+              pie.value(function(d) {                                 // NEW
+                if (d.Country === Country) d.enabled = enabled;           // NEW
+                return (d.enabled) ? d.Value : 0;                     // NEW
+              });                                                     // NEW
 
-var radius = Math.min(width, height) / 2;
-var donutWidth = 50;                            // NEW
-var color = d3.scaleOrdinal(d3.schemeCategory20b);
+              path = path.data(pie(dataByCount));                         // NEW
 
-var svg = d3.select("#donut").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+              path.transition()                                       // NEW
+                .duration(750)                                        // NEW
+                .attrTween('d', function(d) {                         // NEW
+                  var interpolate = d3.interpolate(this._current, d); // NEW
+                  this._current = interpolate(0);                     // NEW
+                  return function(t) {                                // NEW
+                    return arc(interpolate(t));                       // NEW
+                  };                                                  // NEW
+                });                                                   // NEW
+            });                                                       // NEW
+            
+          legend.append('text')
+            .attr('x', legendRectSize + legendSpacing)
+            .attr('y', legendRectSize - legendSpacing)
+            .text(function(d) { return d; });
 
-var arc = d3.arc()
-  .innerRadius(radius - donutWidth)             // UPDATED
-  .outerRadius(radius);
-var pie = d3.pie()
-  .value(function(d) { return d.Value; })
-  .sort(null);
-  var g = svg.selectAll(".arc")
-      .data(pie(dataByCount))
-    .enter().append("g")
-      .attr("class", "arc");
-
-        var path = svg.selectAll('path')
-          .data(pie(dataByCount))
-          .enter()
-          .append('path')
-          .attr('d', arc)
-          .attr('fill', function(d, i) {
-            return color(d.data.Country);
         });
-
-function type(d) {
-    d.Value = +d.Value;
-    return d;
-}
-
- var legend = svg.selectAll('.legend')                     
-          .data(color.domain())                                   
-          .enter()                                                
-          .append('g')                                            
-          .attr('class', 'legend')                                
-          .attr('transform', function(d, i) {                     
-            var height = legendRectSize + legendSpacing;          
-            var offset =  height * color.domain().length / 2;     
-            var horz = -2 * legendRectSize;                       
-            var vert = i * height - offset;                       
-            return 'translate(' + horz + ',' + vert + ')';        
-          });     
-
-legend.append('rect')                                  
-          .attr('width', legendRectSize)                       
-          .attr('height', legendRectSize)                      
-          .style('fill', color)                                
-          .style('stroke', color);                             
-
-        legend.append('text')                                  
-          .attr('x', legendRectSize + legendSpacing)           
-          .attr('y', legendRectSize - legendSpacing)           
-          .text(function(d) { return d; });    
-
-  
-});
+})(window.d3);
